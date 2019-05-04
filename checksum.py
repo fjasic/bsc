ba1624e2a3847b428258906188fdb26013ad1695
Filip Jasic <filipjasic8@gmail.com>
2019-05-04 14:41:02 +0200

    start

---
+++ checksum.py
@@ -0,0 +1,46 @@
+import sys
+
+
+def main():
+    args = sys.argv[1:]
+
+    if len(args) != 3:
+        print "usage: checksum.py <string> <id> <lin_ver>"
+        sys.exit(1)
+
+    ime_string = args[0]
+    id_unos = args[1]
+    lin_ver = args[2]
+
+    data = [ord(c) for c in ime_string]
+    print "hex representation of string:",
+    for i in data:
+        print hex(i),
+    print " "
+    chcksum = 0
+    if lin_ver == "2.0":
+        for i in data:
+            chcksum += i
+            if chcksum >= 256:
+                chcksum -= 255
+    else:
+        for i in data:
+            chcksum += i
+
+    chcksum = 0xff & (~chcksum)
+    print "checksum: " + hex(chcksum)
+    id_string = str(bin(int(id_unos, 16))[2:].zfill(8))
+    print "id: " + id_string
+    id = [ord(c) for c in id_string]
+    p0 = id[0] ^ id[1] ^ id[2] ^ id[4]
+    p1 = not(id[1] ^ id[3] ^ id[4] ^ id[5])
+    if p1 is True:
+        p1 = 1
+    else:
+        p1 = 0
+
+    print "parity: 0b" + str(p1) + str(p0)
+
+
+if __name__ == '__main__':
+    main()
