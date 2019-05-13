b06155e83a1527d4d65c1c57972d5db5d22d692a
Filip Jasic <fjasic@domain.local>
2019-05-13 15:31:15 +0200

    total clean up,useful can signal captured and parsed in csv

--- scope/readout.py
+++ scope/readout.py
@@ -1,64 +1,72 @@
-import time as Time
-import serial
+import visa
+import numpy as np
+from struct import unpack
+import pylab
 import csv
-from TektronixClasses import Tektronix4104
-from checksum import checksum
 import itertools
+import serial
+import sys
+import checksum
 
 
 def main():
     serial_call()
     try:
-        ID = 'USB::0X0699::0x0401::C021046::INSTR'
-        oscope = Tektronix4104(ID)
-        print "Getting offsets..."
-        oscope.getOffsets()
-        armed = oscope.armed
-        auto = oscope.auto
-        ready = oscope.ready
-        save = oscope.save
-        trigger = oscope.trigger
+        scope = visa.ResourceManager().open_resource(
+            'USB::0X0699::0x0401::C021046::INSTR')
+        scope.write('DATA:SOU CH1')
+        scope.write('DATA:WIDTH 1')
+        scope.write('DATA:ENC RPB')
 
-        print "Checking trigger status..."
-        state = oscope.checkTrigger()
+        ymult = float(scope.ask('WFMPRE:YMULT?'))
+        yzero = float(scope.ask('WFMPRE:YZERO?'))
+        yoff = float(scope.ask('WFMPRE:YOFF?'))
+        xincr = float(scope.ask('WFMPRE:XINCR?'))
 
-        triggerCount = 0
-        checkCount = 1
-        start = Time.time()
-        pw_global_index = 0
-        pw_global = 0
-        timeTotal = []
-        voltageTotal = []
-        print "start time: " + str(start)
-        while(Time.time() - start < 5):
-            # while(True):
-            #    if(oscope.state == oscope.trigger):
-            print Time.time()
-            triggerCount += 1
-            print "Scope has triggered %d/%d, reading out data now" % (
-                triggerCount, checkCount)
-            print "raw data: " + str(oscope.readData())
-            [time, volts] = oscope.unpackData()
-            timeTotal = time
+        scope.write('CURVE?')
+        data = scope.read_raw()
 
-            voltageTotal = volts
-            print "Time: " + str(time) + ";" + "Voltage: " + str(volts)
-            if (oscope.state == oscope.auto):
-                oscope.setTriggerNorm()
+        headerlen = 2 + int(data[1])
+        header = data[:headerlen]
+        ADC_wave = data[headerlen:-1]
 
-        state = oscope.checkTrigger()
-        checkCount += 1
-        with open("can-f1ca-ch2-l.csv", "w") as csvFile:
-            csvWriter = csv.writer(csvFile)
-        for val in itertools.izip(timeTotal, voltageTotal):
-            csvWriter.writerow(val)
+        ADC_wave = np.array(unpack('%sB' % len(ADC_wave), ADC_wave))
+        volts = (ADC_wave - yoff)*ymult + yzero
+        time = np.arange(0, xincr*len(volts), xincr)
 
+        for i in range(len(volts)):
+            if volts[i] < 1.0:
+                pw_index = i
+                break
+        reverse_volts = volts[::-1]
+
+        for i in range(len(reverse_volts)):
+            if reverse_volts[i] < 2.0:
+                pw_index_reverse = i
+                break
+
+        pw_index_end = len(volts) - pw_index_reverse
+        volt_usefull = volts[pw_index:pw_index_end]
+        time_usefull = time[pw_index:pw_index_end]
+
+        with open("can-f1ca-ch2-l.csv", "w") as csvCapture:
+            csvWriter = csv.writer(csvCapture)
+            for val in itertools.izip(time_usefull, volt_usefull):
+                csvWriter.writerow(val)
+            print "CSV output done"
+            
+        pylab.plot(time_usefull, volt_usefull)
+        pylab.savefig("can-f1ca-ch2-l.png")
+        print "PNG output done"
+        print "Exit figure to end program..."
+        pylab.show()
+    
     except KeyboardInterrupt:
         print "Done taking data."
 
     finally:
         print "Closing..."
-        # oscope.rm.close()
+        sys.exit()
 
     return 0
 
@@ -80,7 +88,6 @@ def serial_call():
     com3.write("CAN USER OPEN CH2 100K\r\n")
     com4.write("CAN USER OPEN CH2 100K\r\n")
     com4.write("TX2 F1CA\r\n")
-
-
+    
 if __name__ == "__main__":
     main()
