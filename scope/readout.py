c1872607988ec48669add1101d1074b3fe4875fd
Filip Jasic <fjasic@domain.local>
2019-05-20 16:27:37 +0200

    added support for SPI bus

--- scope/readout.py
+++ scope/readout.py
@@ -7,87 +7,152 @@ import itertools
 import serial
 import sys
 import checksum
+# import sshrpi
+"""sve pod komentarom je za CAN"""
 
 
-def main():
-    serial_call()
-    try:
-        scope = visa.ResourceManager().open_resource(
-            'USB::0X0699::0x0401::C021046::INSTR')
-        scope.write('DATA:SOU CH1')
-        scope.write('DATA:WIDTH 1')
-        scope.write('DATA:ENC RPB')
+def main(channel_num):
+    # serial_call()
+    # sshrpi()
+    scope = visa.ResourceManager().open_resource(
+        'USB0::0X0699::0x0401::C021046::INSTR')
+    set_channel(scope, str(channel_num))
+    scope.write('DATA:WIDTH 1')
+    scope.write('DATA:ENC RPB')
 
-        ymult = float(scope.ask('WFMPRE:YMULT?'))
-        yzero = float(scope.ask('WFMPRE:YZERO?'))
-        yoff = float(scope.ask('WFMPRE:YOFF?'))
-        xincr = float(scope.ask('WFMPRE:XINCR?'))
+    ymult = float(scope.ask('WFMPRE:YMULT?'))
+    yzero = float(scope.ask('WFMPRE:YZERO?'))
+    yoff = float(scope.ask('WFMPRE:YOFF?'))
+    xincr = float(scope.ask('WFMPRE:XINCR?'))
 
-        scope.write('CURVE?')
-        data = scope.read_raw()
+    scope.write('CURVE?')
+    data = scope.read_raw()
 
-        headerlen = 2 + int(data[1])
-        header = data[:headerlen]
-        ADC_wave = data[headerlen:-1]
+    headerlen = 2 + int(data[1])
+    header = data[:headerlen]
+    ADC_wave = data[headerlen:-1]
 
-        ADC_wave = np.array(unpack('%sB' % len(ADC_wave), ADC_wave))
-        volts = (ADC_wave - yoff)*ymult + yzero
-        time = np.arange(0, xincr*len(volts), xincr)
-
+    ADC_wave = np.array(unpack('%sB' % len(ADC_wave), ADC_wave))
+    volts = (ADC_wave - yoff)*ymult + yzero
+    global time
+    time = np.arange(0, xincr*len(volts), xincr)
+    global data_volts
+    data_volts = []
+    global clock_volts
+    clock_volts = []
+    pylab.title("SPI")
+    pylab.figure(1)
+    if channel_num == 1:
         for i in range(len(volts)):
-            if volts[i] < 1.0:
-                pw_index = i
-                break
-        reverse_volts = volts[::-1]
+            if volts[i] > 3.0:
+                volts[i] = 1.0
+            else:
+                volts[i] = 0.0
+        data_volts = volts
+        plot_1(time, data_volts)
+    else:
+        for i in range(len(volts)):
+            if volts[i] > 0.0:
+                volts[i] = 1.0
+            else:
+                volts[i] = 0.0
+        clock_volts = volts
+        plot_2(time, clock_volts)
 
-        for i in range(len(reverse_volts)):
-            if reverse_volts[i] < 2.0:
-                pw_index_reverse = i
-                break
+    # for i in range(len(volts)):
+    #     if volts[i] < 4.80:
+    #         pw_index = i
+    #         break
+    # reverse_volts = volts[::-1]
 
-        pw_index_end = len(volts) - pw_index_reverse
-        volt_usefull = volts[pw_index:pw_index_end]
-        time_usefull = time[pw_index:pw_index_end]
+    # for i in range(len(reverse_volts)):
+    #     if reverse_volts[i] < 1.43:
+    #         pw_index_reverse = i
+    #         break
 
-        with open("can-f1ca-ch2-l.csv", "w") as csvCapture:
-            csvWriter = csv.writer(csvCapture)
-            for val in itertools.izip(time_usefull, volt_usefull):
-                csvWriter.writerow(val)
-            print "CSV output done"
-            
-        pylab.plot(time_usefull, volt_usefull)
-        pylab.savefig("can-f1ca-ch2-l.png")
-        print "PNG output done"
-        print "Exit figure to end program..."
-        pylab.show()
-    
-    except KeyboardInterrupt:
-        print "Done taking data."
+    # pw_index_end = len(volts) - pw_index_reverse
+    # volts_useful = volts[pw_index:pw_index_end]
+    # time_useful = time[pw_index:pw_index_end]
 
-    finally:
-        print "Closing..."
-        sys.exit()
+    # can_final = []
 
-    return 0
+    # for i in volts_useful[::40]:
+    #     can_final.append(int(i))
+    # print can_final
+    # for i in range(1, 12):
+    #     id_base += str(can_final[i])
+    # print can_final
+    # print len(can_final)
+    # print "id:" + str(can_final[1:12])
+    return data_volts, clock_volts
 
 
-def serial_call():
-    # data = checksum()
-    com3 = serial.Serial(port="COM3", baudrate=115200)
-    com4 = serial.Serial(port="COM4", baudrate=115200)
-    # com3.write("LIN CLOSE\r\n")
-    # com4.write("LIN CLOSE\r\n")
-    # com3.write("LIN OPEN FREE 1000\r\n")
-    # com4.write("LIN OPEN FREE 1000\r\n")
-    # com3.write("LIN TX %s\r\n" % (data))
-    # print "Sent LIN TX %s\r\n" % (data)
+def decode(data, clock, time):
+    print data
+    print time
+    print clock
 
-    com3.write("CAN USER CLOSE CH2\r\n")
-    com4.write("CAN USER CLOSE CH2\r\n")
 
-    com3.write("CAN USER OPEN CH2 100K\r\n")
-    com4.write("CAN USER OPEN CH2 100K\r\n")
-    com4.write("TX2 F1CA\r\n")
-    
+def plot_1(time, data_volts):
+    pylab.subplot(2, 1, 1)
+    pylab.ylabel("data")
+    # pylab.xlabel("time")
+    pylab.plot(time, data_volts, color="r")
+
+
+def plot_2(time, clock_volts):
+    pylab.subplot(2, 1, 2)
+    pylab.xlabel("time")
+    pylab.ylabel("clock")
+    pylab.plot(time, clock_volts)
+
+
+def plot_everything():
+    pylab.savefig("SPI_scope_output.png")
+    print "PNG output done"
+    print "Exit figure to end program..."
+    pylab.show()
+
+
+def csv_everything(data_final, clock_final):
+    with open("spi-capture.csv", "w") as csvCapture:
+        csvWriter = csv.writer(csvCapture)
+        for val in itertools.izip(time, clock_final, data_final):
+            csvWriter.writerow(val)
+    print "CSV output done"
+
+
+def set_channel(scope, channel):
+    scope.write('DATA:SOU CH'+str(channel))
+
+# def serial_call():
+#     # data = checksum()
+#     com3 = serial.Serial(port="COM3", baudrate=115200)
+#     com4 = serial.Serial(port="COM4", baudrate=115200)
+#     # com3.write("LIN CLOSE\r\n")
+#     # com4.write("LIN CLOSE\r\n")
+#     # com3.write("LIN OPEN FREE 1000\r\n")
+#     # com4.write("LIN OPEN FREE 1000\r\n")
+#     # com3.write("LIN TX %s\r\n" % (data))
+#     # print "Sent LIN TX %s\r\n" % (data)
+
+#     com3.write("CAN USER CLOSE CH2\r\n")
+#     com4.write("CAN USER CLOSE CH2\r\n")
+#     com3.write("CAN USER CLOSE CH1\r\n")
+#     com4.write("CAN USER CLOSE CH1\r\n")
+#     com3.write("CAN USER OPEN CH1 125K\r\n")
+#     com4.write("CAN USER OPEN CH1 125K\r\n")
+#     com3.write("CAN USER OPEN CH2 125K\r\n")
+#     com4.write("CAN USER OPEN CH2 125K\r\n")
+#     com4.write("CAN USER MASK CH2 0000\r\n")
+#     com4.write("CAN USER FILTER CH2 ")
+#     com4.write("CAN USER ALIGN RIGHT\r\n")
+#     com4.write("CAN USER TX CH2 AAAA\r\n")
+
+
 if __name__ == "__main__":
-    main()
+    data_final, _ = main(1)
+    _, clock_final = main(3)
+    plot_everything()
+    decode(data_final, clock_final, time)
+    csv_everything(data_final, clock_final)
---
+++ scope/sshrpi.py
@@ -0,0 +1,19 @@
+import paramiko
+
+
+def Connect(server, port, user, password):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port, user, password)
+    if client is None:
+        return None
+    else:
+        return client
+
+
+def ssh_call():
+    client = Connect("169.254.2.187", port=22, user="pi", password="pi")
+    client.exec_command("python SPI-Test/readSpi.py\n")
+    client.close()
+
